e8bb4be64d6ec10e7c8567c1039e7e463e8eda54
daniel-pan <pandazhao2012@gmail.com>
2015-10-28 18:27:33 -0400

    Update factorial.c

--- factorial.c
+++ factorial.c
@@ -1,7 +1,6 @@
 #include "stdlib.h"
 #include "stdio.h"
 #include "unistd.h"
-#include "math.h"
 
 int factorial(int n){
         return (n == 1 || n == 0) ? 1 : factorial(n - 1) * n;
@@ -14,7 +13,7 @@ main(int argc, char **argv){
         }
         float size = atof(argv[1]) * 1024 * 1024 * 1024 / 4;
         printf("%f\n", size);
-        int *mem = (int*)malloc(sizeof(int) * size);
+        float *mem = (float*)malloc(sizeof(float) * size);
         while (1) {
                 printf("%d\n", factorial(100));
         }
